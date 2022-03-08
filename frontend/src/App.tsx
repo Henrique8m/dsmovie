46baf33012acb942556e1ab30f9a7ad06be9f566
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-08 12:46:29 -0300

    Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
+import Navbar from "./components/NavBar";
+
 function App() {
-  return (<h1>Pagina Principal</h1>
+  return (
+    <Navbar/>
   );
 }
 
---
+++ frontend/src/components/NavBar/index.tsx
@@ -0,0 +1,23 @@
+import { ReactComponent as Github } from 'assets/img/github.svg';
+import './styles.css'
+
+function Navbar() {
+
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/Henrique8m">
+                        <div className="dsmovie-contact-container">
+                            <Github />
+                            <p className="dsmovie_contact-link">/Henrique8m</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
