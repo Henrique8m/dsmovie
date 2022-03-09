0f8025d24ddf7c1403f86b4e54eb117759055b28
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-09 12:41:32 -0300

    Domain model, database seed

---
+++ backend/src/main/java/com/hrodrigues/dsmovie/entities/Score.java
@@ -0,0 +1,46 @@
+package com.hrodrigues.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_score")
+public class Score implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EmbeddedId
+	private ScorePK id = new ScorePK();
+	private Double value;
+	
+	public Score() {
+		
+	}
+	
+	public void setMovie(Movie movie) {
+		id.setMovie(movie);
+	}
+	
+	public void setUser(User user) {
+		id.setUser(user);
+	}
+	public ScorePK getId() {
+		return id;
+	}
+
+	public void setId(ScorePK id) {
+		this.id = id;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+	
+	
+}
---
+++ backend/src/main/java/com/hrodrigues/dsmovie/entities/User.java
@@ -0,0 +1,43 @@
+package com.hrodrigues.dsmovie.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
+public class User implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	
+	public User() {}
+
+	public User(Long id, String email) {
+		this.id = id;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
