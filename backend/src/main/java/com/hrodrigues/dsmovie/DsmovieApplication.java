7f3008abb9892a5de213b616e718c1243ce86a2d
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-09 12:07:04 -0300

    Security config

--- backend/src/main/java/com/hrodrigues/dsmovie/DsmovieApplication.java
+++ backend/src/main/java/com/hrodrigues/dsmovie/DsmovieApplication.java
@@ -1,4 +1,4 @@
-package com.hrodrigues.dsmovie;
+ package com.hrodrigues.dsmovie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
