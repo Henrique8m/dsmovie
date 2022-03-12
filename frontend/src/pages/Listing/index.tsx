c24f9ea341554d9e3f4600af99a838f6b2050c3f
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-12 15:26:19 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,21 +1,33 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 
 function Listing() {
 
-    //FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-    .then(resposta =>{
+    //Manter estado no componente
+    //userStade sempre iniciar ele com um numero
+    const [pageNumber, setPageNumber] = useState(0);
+
+    //Executar algo na instanciação ou destruição do componente, observar estado
+    //Recebe dois argumentos uma função a ser executada, e uma lista de objetos para um listner
+    useEffect(() => {
+
+        axios.get(`${BASE_URL}/movies?size=12&page=1`).then(resposta =>{
+
         console.log(resposta.data);
+        const data = resposta.data as MoviePage;
+        setPageNumber(data.number);
 
     })
-
+    }, []);
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             
             <div className="container">
