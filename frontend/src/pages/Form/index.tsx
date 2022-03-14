90ecbdf867156c500927fcc252b2a22033d76c40
Henrique Rodrigues <hyck.rocha@gmail.com>
2022-03-13 22:37:56 -0300

    Link, movie delete /pages/Form

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,6 +1,6 @@
 import FormCard from 'components/FormCard';
-import { Link, useParams } from 'react-router-dom';
-import { Movie } from 'types/movie';
+import { useParams } from 'react-router-dom';
+
 
 function Form( ) {
 
